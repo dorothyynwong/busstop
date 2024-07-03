cf9eee5b80e3979e6d805ae13da0f25beb17fe8f
Dorothy Wong <dorothy.wong@softwire.com>
2024-07-03 15:06:09 +0100

    get longitude and latitude

--- busstop.js
+++ busstop.js
@@ -11,20 +11,53 @@ const userInput = "490008660N";
 let urlWithBusstop = url.concat(userInput+"/Arrivals");
 //console.log(urlWithBusstop);
 
+/*let variable;
+try {
+const response = await fetch(“url.com”);
+const variable = response;
+if (response.status !== “200”) {
+throw “Error!”;
+} 
+console.log(variable);
+} catch (error) {
+console.log(error);
+}
+console.log(“Ok!”);*/
+let variable;
   async function fetchData() {
     try {
         const response = await fetch(urlPostcode);
+        variable = response;
+        
         const data = await response.json();
-        return data;
+        
+        return data["result"];
     } catch (error) {
         console.error('Error fetching data:', error);
     }
 }
 
 // Usage
-fetchData().then((result) => {
-    console.log('Fetched data:', result);
-});
+async function processData() {
+    const result = await fetchData();
+    //result.forEach(item => console.log(item.longitude));
+    let longitude = result["longitude"];
+    let latitude = result["latitude"];
+    //console.log(result["longitude"]);
+    console.log(longitude, latitude);
+}
+
+processData();
+
+//result.forEach(item => {console.log(item.longitude)});
+
+
+/*fetchData().then(result => {
+    //console.log('Fetched data:', result);
+    result.forEach(item => {console.log(item.longitude)});
+        
+    });*/
+
 
 
 /*

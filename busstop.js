30ea13759e610c377a4fb1561a22daf68b44bc7a
Purbai Varsani <purbai.varsani@softwire.com>
2024-07-03 11:23:02 +0100

    adding prompt

--- busstop.js
+++ busstop.js
@@ -1,5 +1,13 @@
 const url = "https://api.tfl.gov.uk/StopPoint/490008660N";
 
+const prompt = require("prompt-sync")({ sigint: true });
+const userInput = prompt("Enter bus stop: ");
+
+console.log(userInput);
+
+/*
 fetch(url)
     .then(response => response.json())
     .then(body => console.log(body));
+
+    */

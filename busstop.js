1e664c99968e4f8f906ddb1c383e36458c222ef0
Purbai Varsani <purbai.varsani@softwire.com>
2024-07-03 11:29:51 +0100

    URL and input combine

--- busstop.js
+++ busstop.js
@@ -1,13 +1,15 @@
-const url = "https://api.tfl.gov.uk/StopPoint/490008660N";
+//const url = "https://api.tfl.gov.uk/StopPoint/490008660N";
+const url = "https://api.tfl.gov.uk/StopPoint/";
 
 const prompt = require("prompt-sync")({ sigint: true });
 const userInput = prompt("Enter bus stop: ");
 
-console.log(userInput);
+//console.log(userInput);
 
-/*
-fetch(url)
+let urlWithBusstop = url.concat(userInput);
+//console.log(urlWithBusstop);
+
+fetch(urlWithBusstop)
     .then(response => response.json())
     .then(body => console.log(body));
 
-    */
